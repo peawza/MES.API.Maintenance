e27254dfd3bc7585e97862af2ff72007733316e8
Supachai <supachai@CSIGROUPS.COM>
2025-02-05 11:44:10 +0700

    add Model Connect DB

---
+++ MaintenanceMasterPostgreSQLDB/ProductionDbContext.cs
@@ -0,0 +1,27 @@
+﻿namespace Application
+{
+    public class SystemDbContext : DbContext
+    {
+
+
+        public SystemDbContext(DbContextOptions<SystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        public static void InitialService(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<SystemDbContext>(options =>
+                options.UseNpgsql(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("public");
+
+
+        }
+    }
+}
